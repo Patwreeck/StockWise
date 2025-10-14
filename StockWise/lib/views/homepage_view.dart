65b64dbb56ee9d125f37e2b7f51c5c116f3331b7
Patwreeck <blackforest.saints@gmail.com>
2025-10-14 20:14:24 +0800

    refactor

--- StockWise/lib/views/homepage_view.dart
+++ StockWise/lib/views/homepage_view.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import '../controllers/homepage_controller.dart';
 
-// 🆕 Import the other view pages
+// Import the other view pages
 import 'outofstock_view.dart';
 import 'lowstock_view.dart';
 import 'history_view.dart';
@@ -28,9 +28,11 @@ class HomePageView extends StatelessWidget {
                 "Are you sure you want to logout?",
                 textAlign: TextAlign.center,
                 style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.w600,
-                    color: Colors.black87),
+                  fontFamily: "Poppins",
+                  fontSize: 16,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.black87,
+                ),
               ),
               const SizedBox(height: 24),
               Row(
@@ -44,7 +46,7 @@ class HomePageView extends StatelessWidget {
                     ),
                     onPressed: () {
                       Navigator.pop(context); // close dialog
-                      Get.offAllNamed("/login"); // NOT WORKING ~ TO CHECK 
+                      Get.offAllNamed("/login"); // now this should work
                     },
                     child: const Text("Yes, I am sure",
                         style: TextStyle(color: Colors.white)),
@@ -78,12 +80,13 @@ class HomePageView extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            // Top bar
+            // 🔝 Top bar
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
+                  // Optional black logo container
                   Container(
                     height: 32,
                     width: 32,
@@ -112,181 +115,180 @@ class HomePageView extends StatelessWidget {
 
             const SizedBox(height: 20),
 
-            // Grid of cards
+            // 🔲 Grid with 2 rows x 2 columns
             Expanded(
-              child: GridView.count(
-                crossAxisCount: 2,
-                padding: const EdgeInsets.all(16),
-                crossAxisSpacing: 16,
-                mainAxisSpacing: 16,
-                children: [
-                  // Out of Stock
-                  GestureDetector(
-                    onTap: () => Get.to(() => const OutOfStockView()),
-                    child: Obx(() => Container(
-                          decoration: BoxDecoration(
-                            color: Colors.pinkAccent,
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                          child: Stack(
-                            children: [
-                              Padding(
-                                padding: const EdgeInsets.all(16),
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      controller.outOfStock.value.toString(),
-                                      style: const TextStyle(
-                                        fontSize: 32,
-                                        fontWeight: FontWeight.bold,
-                                        color: Colors.white,
-                                      ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 35), // match Trello padding
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        // Out of Stock card
+                        Expanded(
+                          child: GestureDetector(
+                            onTap: () => Get.to(() => const OutOfStockView()),
+                            child: Obx(() => Container(
+                                  height: 150,
+                                  margin: const EdgeInsets.all(8),
+                                  decoration: BoxDecoration(
+                                    color: const Color(0xFFFF2E63),
+                                    borderRadius: BorderRadius.circular(20),
+                                  ),
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(16),
+                                    child: Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          controller.outOfStock.value.toString(),
+                                          style: const TextStyle(
+                                            fontFamily: "Poppins",
+                                            fontSize: 48,
+                                            fontWeight: FontWeight.w500,
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                        const Spacer(),
+                                        const Text(
+                                          "Out of stock",
+                                          style: TextStyle(
+                                            fontFamily: "Poppins",
+                                            fontSize: 14,
+                                            fontWeight: FontWeight.w500,
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                      ],
                                     ),
-                                    const Spacer(),
-                                    const Text(
-                                      "Out of stock",
-                                      style: TextStyle(
-                                        fontSize: 14,
-                                        fontWeight: FontWeight.w500,
-                                        color: Colors.white,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                              const Positioned(
-                                right: 12,
-                                top: 12,
-                                child: Icon(Icons.error, color: Colors.white),
-                              ),
-                            ],
+                                  ),
+                                )),
                           ),
-                        )),
-                  ),
+                        ),
 
-                  // Low Stock
-                  GestureDetector(
-                    onTap: () => Get.to(() => const LowStockView()),
-                    child: Obx(() => Container(
-                          decoration: BoxDecoration(
-                            color: Colors.black87,
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                          child: Stack(
-                            children: [
-                              Padding(
-                                padding: const EdgeInsets.all(16),
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      controller.lowStock.value.toString(),
-                                      style: const TextStyle(
-                                        fontSize: 32,
-                                        fontWeight: FontWeight.bold,
-                                        color: Colors.white,
-                                      ),
+                        // Low Stock card
+                        Expanded(
+                          child: GestureDetector(
+                            onTap: () => Get.to(() => const LowStockView()),
+                            child: Obx(() => Container(
+                                  height: 150,
+                                  margin: const EdgeInsets.all(8),
+                                  decoration: BoxDecoration(
+                                    color: const Color(0xFF252A34),
+                                    borderRadius: BorderRadius.circular(20),
+                                  ),
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(16),
+                                    child: Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          controller.lowStock.value.toString(),
+                                          style: const TextStyle(
+                                            fontFamily: "Poppins",
+                                            fontSize: 48,
+                                            fontWeight: FontWeight.w500,
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                        const Spacer(),
+                                        const Text(
+                                          "Low stock",
+                                          style: TextStyle(
+                                            fontFamily: "Poppins",
+                                            fontSize: 14,
+                                            fontWeight: FontWeight.w500,
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                      ],
                                     ),
-                                    const Spacer(),
-                                    const Text(
-                                      "Low stock",
-                                      style: TextStyle(
-                                        fontSize: 14,
-                                        fontWeight: FontWeight.w500,
-                                        color: Colors.white,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                              const Positioned(
-                                right: 12,
-                                top: 12,
-                                child: Icon(Icons.check_circle,
-                                    color: Colors.green),
-                              ),
-                            ],
+                                  ),
+                                )),
                           ),
-                        )),
-                  ),
-
-                  // History
-                  GestureDetector(
-                    onTap: () => Get.to(() => const AllProductsView()),
-                    child: Obx(() => Container(
-                          decoration: BoxDecoration(
-                            color: Colors.cyanAccent,
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                          child: Stack(
-                            children: [
-                              Padding(
-                                padding: const EdgeInsets.all(16),
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      controller.totalItems.value.toString(),
-                                      style: const TextStyle(
-                                        fontSize: 32,
-                                        fontWeight: FontWeight.bold,
-                                        color: Colors.black,
-                                      ),
-                                    ),
-                                    const Spacer(),
-                                    const Text(
-                                      "History",
-                                      style: TextStyle(
-                                        fontSize: 14,
-                                        fontWeight: FontWeight.w500,
-                                        color: Colors.black,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                              const Positioned(
-                                right: 12,
-                                top: 12,
-                                child:
-                                    Icon(Icons.flash_on, color: Colors.black),
-                              ),
-                            ],
-                          ),
-                        )),
-                  ),
-
-                  // All Products
-                  GestureDetector(
-                    onTap: () => Get.to(() => const HistoryView()),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: Colors.grey.shade300,
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      child: Stack(
-                        children: [
-                          const Center(
-                            child: Icon(Icons.list,
-                                color: Colors.black, size: 48),
-                          ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
-                  ),
-                ],
+
+                    Row(
+                      children: [
+                        // All Products card
+                        Expanded(
+                          child: GestureDetector(
+                            onTap: () => Get.to(() => const AllProductsView()),
+                            child: Obx(() => Container(
+                                  height: 150,
+                                  margin: const EdgeInsets.all(8),
+                                  decoration: BoxDecoration(
+                                    color: const Color(0xFF08D9D6),
+                                    borderRadius: BorderRadius.circular(20),
+                                  ),
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(16),
+                                    child: Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          controller.totalItems.value.toString(),
+                                          style: const TextStyle(
+                                            fontFamily: "Poppins",
+                                            fontSize: 48,
+                                            fontWeight: FontWeight.w500,
+                                            color: Colors.black,
+                                          ),
+                                        ),
+                                        const Spacer(),
+                                        const Text(
+                                          "All products",
+                                          style: TextStyle(
+                                            fontFamily: "Poppins",
+                                            fontSize: 14,
+                                            fontWeight: FontWeight.w500,
+                                            color: Colors.black,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                )),
+                          ),
+                        ),
+
+                        // History card
+                        Expanded(
+                          child: GestureDetector(
+                            onTap: () => Get.to(() => const HistoryView()),
+                            child: Container(
+                              height: 150,
+                              margin: const EdgeInsets.all(8),
+                              decoration: BoxDecoration(
+                                color: Colors.grey.shade300,
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              child: const Center(
+                                child: Icon(Icons.list,
+                                    color: Colors.black, size: 48),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
 
-            // Last login
+            // ⏱ Last login
             Padding(
               padding: const EdgeInsets.all(12),
               child: Obx(() => Center(
                     child: Text(
                       "Last login, ${controller.lastLogin.value}",
                       style: const TextStyle(
-                          fontSize: 12, color: Colors.black87),
+                        fontFamily: "Poppins",
+                        fontSize: 12,
+                        color: Colors.black87,
+                      ),
                     ),
                   )),
             ),
